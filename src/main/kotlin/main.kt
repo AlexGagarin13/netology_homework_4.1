07f518706c52456bfdf4fad059ad0313b19031ee
Aleksey Anokhin <anoxin-aleksey@bk.ru>
2022-03-17 11:57:26 +0300

    All needed tests are added

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -12,11 +12,11 @@ fun main() {
 
     val accountType1 = "MASTERCARD/MAESTRO"
     val accountType2 = "VISA/MIR"
-    val previousTransfersInMonthInPennies1 = 66_000_00.0
+    val previousTransfersInMonthInPennies1 = 50_000_00.0
     val previousTransfersInMonthInPennies2 = 40_000_00.0
     val transferAmountInPennies1 = 11_000_00.0
     val transferAmountInPennies2 = 50_023_50.0
-    val transferAmountInPennies3 = 41_000_00.0
+    val transferAmountInPennies3 = 5_000_00.0
     val df = DecimalFormat("#.##")
 
     println(
@@ -59,22 +59,20 @@ fun calculateCommission(
     transferAmountInPennies: Double
 ): Double {
     val commissionToPay: Double = when (accountType) {
-        "VK Pay" -> if (previousTransfersInMonthInPennies <= VK_PAY_LIMIT) {
-            if (transferAmountInPennies <= VK_PAY_LIMIT) {
-                0.0
+        "VK Pay" -> if (previousTransfersInMonthInPennies <= VK_PAY_LIMIT && previousTransfersInMonthInPennies + transferAmountInPennies <= VK_PAY_LIMIT) {
+                            0.0
             } else error("Месячный лимит для перевода VK Pay превышен!")
-        } else error("Месячный лимит для перевода через VK Pay превышен!")
-        "MASTERCARD/MAESTRO" -> if (previousTransfersInMonthInPennies <= CARDS_TRANSFER_LIMIT) {
+        "MASTERCARD/MAESTRO" -> if (previousTransfersInMonthInPennies <= CARDS_TRANSFER_LIMIT && previousTransfersInMonthInPennies + transferAmountInPennies <= CARDS_TRANSFER_LIMIT) {
             if (transferAmountInPennies >= LIMIT_WITHOUT_COMMISSION_MASTERCARD_MAESTRO || previousTransfersInMonthInPennies + transferAmountInPennies >= LIMIT_WITHOUT_COMMISSION_MASTERCARD_MAESTRO) {
                 (transferAmountInPennies * COMMISSION_MASTERCARD_MAESTRO) + COMMISSION_MASTERCARD_MAESTRO_FIX
             } else 0.0
-        } else error("Месячный лимит для перевода превышен!")
-        "VISA/MIR" -> if (previousTransfersInMonthInPennies <= CARDS_TRANSFER_LIMIT) {
+        } else error("Месячный лимит для перевода по картам  превышен!")
+        "VISA/MIR" -> if (previousTransfersInMonthInPennies <= CARDS_TRANSFER_LIMIT && previousTransfersInMonthInPennies + transferAmountInPennies <= CARDS_TRANSFER_LIMIT) {
             if (transferAmountInPennies * COMMISSION_VISA_MIR < MINIMUM_COMMISSION_VISA_MIR) {
-                (transferAmountInPennies * COMMISSION_VISA_MIR) + MINIMUM_COMMISSION_VISA_MIR
+                MINIMUM_COMMISSION_VISA_MIR
             } else transferAmountInPennies * COMMISSION_VISA_MIR
-        } else error("Месячный лимит для перевода превышен!")
-        else -> 0.0
+        } else error("Месячный лимит для перевода по картам превышен!")
+        else -> error("Платежная система не поддерживается")
     }
     return commissionToPay / 100
 }
--- src/test/kotlin/MainKtTest.kt
+++ src/test/kotlin/MainKtTest.kt
@@ -1,29 +1,132 @@
+import org.junit.Assert.assertEquals
 import org.junit.Test
-
-import org.junit.Assert.*
-import java.text.DecimalFormat
+import java.lang.IllegalStateException
 
 class MainKtTest {
 
     @Test
     fun calculateCommission_MASTERCARD_MAESTRO_with_commission() {
-//        arrange
-        val LIMIT_WITHOUT_COMMISSION_MASTERCARD_MAESTRO = 75_000_00.0
-        val COMMISSION_MASTERCARD_MAESTRO = 0.006
-        val COMMISSION_MASTERCARD_MAESTRO_FIX = 20_00.0
         val accountType1 = "MASTERCARD/MAESTRO"
         val previousTransfersInMonthInPennies1 = 66_000_00.0
         val transferAmountInPennies1 = 11_000_00.0
-        val df = DecimalFormat("#.##")
 
-//        act
         val result = calculateCommission(
             accountType = accountType1,
             previousTransfersInMonthInPennies = previousTransfersInMonthInPennies1,
             transferAmountInPennies = transferAmountInPennies1
         )
 
-        //        assert
         assertEquals(86.0, result, 0.00001)
     }
+
+    @Test
+    fun calculateCommission_MASTERCARD_MAESTRO_without_commission() {
+        val accountType1 = "MASTERCARD/MAESTRO"
+        val previousTransfersInMonthInPennies1 = 66_000_00.0
+        val transferAmountInPennies1 = 5_000_00.0
+
+        val result = calculateCommission(
+            accountType = accountType1,
+            previousTransfersInMonthInPennies = previousTransfersInMonthInPennies1,
+            transferAmountInPennies = transferAmountInPennies1
+        )
+
+        assertEquals(0.0, result, 0.00001)
+    }
+
+    @Test(expected = IllegalStateException::class)
+    fun out_of_cards_limit_MASTERCARD_MAESTRO() {
+        val accountType1 = "MASTERCARD/MAESTRO"
+        val previousTransfersInMonthInPennies1 = 100_000_00.0
+        val transferAmountInPennies1 = 550_000_00.0
+
+        val result = calculateCommission(
+            accountType = accountType1,
+            previousTransfersInMonthInPennies = previousTransfersInMonthInPennies1,
+            transferAmountInPennies = transferAmountInPennies1
+        )
+    }
+
+    @Test
+    fun calculateCommission_VISA_MIR() {
+        val accountType1 = "VISA/MIR"
+        val previousTransfersInMonthInPennies1 = 10_000_00.0
+        val transferAmountInPennies1 = 10_000_00.0
+
+        val result = calculateCommission(
+            accountType = accountType1,
+            previousTransfersInMonthInPennies = previousTransfersInMonthInPennies1,
+            transferAmountInPennies = transferAmountInPennies1
+        )
+
+        assertEquals(75.0, result, 0.00001)
+    }
+
+    @Test
+    fun calculateCommission_VISA_MIR_MINIMUM_COMMISSION() {
+        val accountType1 = "VISA/MIR"
+        val previousTransfersInMonthInPennies1 = 10_000_00.0
+        val transferAmountInPennies1 = 1_000_00.0
+
+        val result = calculateCommission(
+            accountType = accountType1,
+            previousTransfersInMonthInPennies = previousTransfersInMonthInPennies1,
+            transferAmountInPennies = transferAmountInPennies1
+        )
+
+        assertEquals(35.0, result, 0.00001)
+    }
+
+    @Test(expected = IllegalStateException::class)
+    fun out_of_cards_limit_VISA_MIR() {
+        val accountType1 = "VISA/MIR"
+        val previousTransfersInMonthInPennies1 = 500_000_00.0
+        val transferAmountInPennies1 = 150_000_00.0
+
+        val result = calculateCommission(
+            accountType = accountType1,
+            previousTransfersInMonthInPennies = previousTransfersInMonthInPennies1,
+            transferAmountInPennies = transferAmountInPennies1
+        )
+    }
+
+    @Test
+    fun calculateCommission_VK_PAY() {
+        val accountType1 = "VK Pay"
+        val previousTransfersInMonthInPennies1 = 10_000_00.0
+        val transferAmountInPennies1 = 1_000_00.0
+
+        val result = calculateCommission(
+            accountType = accountType1,
+            previousTransfersInMonthInPennies = previousTransfersInMonthInPennies1,
+            transferAmountInPennies = transferAmountInPennies1
+        )
+
+        assertEquals(0.0, result, 0.00001)
+    }
+
+    @Test(expected = IllegalStateException::class)
+    fun out_of_limit_VK_PAY() {
+        val accountType1 = "VK Pay"
+        val previousTransfersInMonthInPennies1 = 41_000_00.0
+        val transferAmountInPennies1 = 1_000_00.0
+
+        val result = calculateCommission(
+            accountType = accountType1,
+            previousTransfersInMonthInPennies = previousTransfersInMonthInPennies1,
+            transferAmountInPennies = transferAmountInPennies1
+        )
+    }
+
+    @Test(expected = IllegalStateException::class)
+    fun illegal_pay_system() {
+        val accountType1 = "Pay Pal"
+        val transferAmountInPennies1 = 1_000_00.0
+
+        val result = calculateCommission(
+            accountType = accountType1,
+            transferAmountInPennies = transferAmountInPennies1
+        )
+    }
 }
+
